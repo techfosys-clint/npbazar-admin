4e20d2b1ff8a05a25f5d7c6f0a054051ea3f79b5
Shahriar-Fardows <shahriarfardows@gmail.com>
2026-07-24 08:39:04 +0600

    Auto-recover from stale chunk errors after deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import ChunkErrorHandler from "@/components/ChunkErrorHandler";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -45,7 +46,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ChunkErrorHandler />
+        {children}
+      </body>
     </html>
   );
 }
